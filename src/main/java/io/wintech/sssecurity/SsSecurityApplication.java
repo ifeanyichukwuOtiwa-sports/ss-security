5204b64b7e1a02b65c12211bd3cf00c8e8facee9
Ifeanyichukwu Otiwa <ifeanyichukwu.otiwa@betpawa.com>
2024-02-17 16:02:13 +0100

    [Security] ** initial commit

---
+++ src/main/java/io/wintech/sssecurity/SsSecurityApplication.java
@@ -0,0 +1,13 @@
+package io.wintech.sssecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/wintech/sssecurity/SsSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package io.wintech.sssecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
